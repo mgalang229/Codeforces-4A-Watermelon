b8024909d3739c7888ba364490de28c8d057ffe2
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2020-05-14 20:54:41 +0800

    Add files via upload

---
+++ test.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int n;
+	cin >> n;
+	if(n % 2 == 0 && n > 2){
+		cout << "YES";
+	} else{
+		cout << "NO";
+	}
+	return 0;
+}
